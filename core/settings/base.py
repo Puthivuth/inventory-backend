b20c87e5de4d124dc652cb05d1ab133e69743731
puthivuth <chhoeunputhivuth@gmail.com>
2026-06-11 16:38:01 +0700

    fix db

--- core/settings/base.py
+++ core/settings/base.py
@@ -62,10 +62,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database configuration from environment variables
 # Using dj-database-url to parse DATABASE_URL or fallback to individual env vars
-# DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://mengheang:LInnBh7Kie3EL3gzIaQVkMX0q23Ha77R@dpg-d82s3ov2gups7398ai4g-a.singapore-postgres.render.com/inventory_database_6u51')
-# DATABASES = {
-#     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
-# }
+
 
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
